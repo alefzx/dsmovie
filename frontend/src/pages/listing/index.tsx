0f00ee975eec185b9641e9497d2e98fe3b79b5fd
Alef Souza <alef.souza.j@gmail.com>
2022-09-02 16:45:53 -0300

    MovieCard

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,11 +1,21 @@
+import MovieCard from 'Components/MovieCard';
 import Pagination from 'Components/Pagination';
+
 import React from 'react';
 
 const Listing = () => {
     return (
-        <div>
+        <>
            <Pagination />
-        </div>
+           <div className = "row">
+            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+            </div>
+         
+         
+           </div>
+          
+        </>
     );
 };
 
